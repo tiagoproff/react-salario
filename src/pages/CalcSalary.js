48bae51107b123aea40884e2af9d4b46ffd95ded
Tiago S. Oliveira <TiagoProff@gmail.com>
2020-06-21 12:42:24 -0300

    Input layouts

--- src/pages/CalcSalary.js
+++ src/pages/CalcSalary.js
@@ -4,25 +4,31 @@ function CalcSalary() {
     return (
         <div className="row">
             <form className="col s12">
-                <div className="input-field col s12">
-                    <input id="salariobruto" type="number" min="1" step="10" className="validate" />
-                    <label for="salariobruto">Salário Bruto</label>
+                <div className="row">
+                    <div className="input-field col s9">
+                        <input id="salariobruto" type="number" min="1" step="10" className="validate" />
+                        <label for="salariobruto">Salário Bruto</label>
+                    </div>
+                    <div className="input-field col s3">
+                        <input placeholder="R$ 0.00" id="salarioliquido" type="text" disabled />
+                        <label for="salarioliquido">Salário Líquido</label>
+                    </div>
                 </div>
                 <div className="row">
                     <div className="input-field col s3">
-                        <input placeholder="Base INSS" id="baseinss" type="text" disabled />
+                        <input placeholder="R$ 0.00" id="baseinss" type="text" disabled />
                         <label for="baseinss">Base INSS</label>
                     </div>
                     <div className="input-field col s3">
-                        <input placeholder="Desconto INSS" id="descontoinss" type="text" disabled />
+                        <input placeholder="R$ 0.00" id="descontoinss" type="text" disabled />
                         <label for="descontoinss">Desconto INSS</label>
                     </div>
                     <div className="input-field col s3">
-                        <input placeholder="Base IRPF" id="baseirpf" type="text" disabled />
+                        <input placeholder="R$ 0.00" id="baseirpf" type="text" disabled />
                         <label for="baseirpf">Base IRPF</label>
                     </div>
                     <div className="input-field col s3">
-                        <input placeholder="Desconto IRPF" id="descontoirpf" type="text" disabled />
+                        <input placeholder="R$ 0.00" id="descontoirpf" type="text" disabled />
                         <label for="descontoirpf">Desconto IRPF</label>
                     </div>
                 </div>
