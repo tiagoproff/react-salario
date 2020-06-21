3bc92ffb20b7d6cdb723171fd414ba6a1796cfa8
Tiago S. Oliveira <TiagoProff@gmail.com>
2020-06-21 12:02:50 -0300

    Add title in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import logofile from '../logo.svg';
 
 function Header() {
     return (
-        <nav>
+        <nav className="App-header">
             <div class="nav-wrapper">
-                <a href="#">
-                    <img src={logofile} class="App-logo" alt="logo" />
-                </a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">JavaScript</a></li>
-                </ul>
+                <h2 className="App-title">React Salário</h2>
             </div>
         </nav>
     );
