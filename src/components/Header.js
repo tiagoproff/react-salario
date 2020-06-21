0c322397648601acbcb1d4143b59f1bcf32d4716
Tiago S. Oliveira <TiagoProff@gmail.com>
2020-06-21 20:39:34 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <nav className="App-header">
-            <div class="nav-wrapper">
+            <div className="nav-wrapper">
                 <h2 className="App-title">React Salário</h2>
             </div>
         </nav>
