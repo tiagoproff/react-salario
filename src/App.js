eee166f7aaf3457d6b86e36cfa1a7c9370cddf8b
Tiago S. Oliveira <TiagoProff@gmail.com>
2020-06-21 20:38:36 -0300

    Add EventEmitter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 //import $ from 'jquery';
 import 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
+import { EventEmitter } from './EventEmitter';
 import Header from './components/Header.js';
 import GraphBar from './components/GraphBar.js';
 import CalcSalary from './pages/CalcSalary.js';
@@ -10,25 +11,36 @@ import { getData } from './functions.js';
 import './App.css';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      feeds: getData()
-    };
-  }
+        this.state = {
+            feeds: getData(),
+            salary: 0,
+            dataGraph: []
+        };
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <Header className="App-header" />
-        <CalcSalary />
-        <GraphBar
-          data={this.state.feeds[1].data}
-          title={this.state.feeds[1].title}
-          color="#70CAD1"
-        />
-      </div>
-    );
-  }
+    componentDidMount () {
+        EventEmitter.subscribe('changeSalary', value => this.setState({
+            salary: value
+        }));
+        EventEmitter.subscribe('changeGaphData',  value => this.setState({
+            dataGraph: value
+        }));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header className="App-header" />
+                <CalcSalary salary={this.state.salary} />
+                <GraphBar
+                    data={this.state.dataGraph}
+                    title={this.state.feeds[1].title}
+                    color="#70CAD1"
+                />
+            </div>
+        );
+    }
 }
